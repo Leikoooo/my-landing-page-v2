3f7d911f70c259deab12b8e3cd54f7e4c6d260a4
leikooo <solomon.www@mail.ru>
2024-01-22 08:59:53 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import FirstScreen from './Components/FirstScreen/FirstScreen';
+import MyStack from './Components/MyStack/MyStack';
+
+function App() {
+  return (
+    <div className="App">
+        <FirstScreen/>
+      <div className="Screen">
+        <MyStack/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
