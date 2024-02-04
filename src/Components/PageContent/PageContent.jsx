0ebf4f48cc57bccc09a335e66a9d7eb701ce3897
Никита <solomon.www@mail.ru>
2024-02-04 03:22:01 +0300

    fixed

--- src/Components/PageContent/PageContent.jsx
+++ src/Components/PageContent/PageContent.jsx
@@ -4,10 +4,7 @@ export default function PageContent({PageTitle, Component}) {
     return (
         <div className="pageContent">
             <span className="pageTitle">{PageTitle}</span>
-            <div className="gradient"/>
-
             <div className="Content">
-
                 <Component/>
             </div>
         </div>
