3de5c489446021e1bc1cecd48a1d2391af7859e8
Никита <solomon.www@mail.ru>
2024-02-04 03:30:41 +0300

    fixed

--- src/Components/Separator/Separator.jsx
+++ src/Components/Separator/Separator.jsx
@@ -1,29 +1,6 @@
 import './Separator.scss';
-import { useState, useEffect } from 'react';
-function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-        width,
-        height
-    };
-}
-
-function useWindowDimensions() {
-    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-    useEffect(() => {
-        function handleResize() {
-            setWindowDimensions(getWindowDimensions());
-        }
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-    return windowDimensions;
-}
-
 
 export default function Separator({TextLine}) {
-    const { height, width } = useWindowDimensions();
-    console.log(width)
     return (
         <div className="separator">
             <section className="separator__wave">
